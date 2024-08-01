010f962f3fe1523de771cd9725201e6aa86260ae
Juan Cruz Cordoneda <71473532+JuanCordoneda@users.noreply.github.com>
2024-08-01 09:27:40 -0300

    bearer token

--- app/app.py
+++ app/app.py
@@ -3,8 +3,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config.config import Config
+from flask_httpauth import HTTPTokenAuth
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+# Configurar Flask-HTTPAuth
+auth = HTTPTokenAuth(scheme='Bearer')
+
+# Tokens válidos (esto es solo un ejemplo, en producción deberías gestionar los tokens de forma segura)
+TOKENS = {
+    "7a2b3c4d5e6f7081920a1b2c3d4e5f6g": "user1",
+    "0a1b2c3d4e5f6071829a1b2c3d4e5f6g": "user2"
+}
+
+@auth.verify_token
+def verify_token(token):
+    if token in TOKENS:
+        return TOKENS[token]
+    return None
+
+@auth.error_handler
+def auth_error(status):
+    return {"message": "Access denied"}, status
+
 
--- app/routes/entidades_routes.py
+++ app/routes/entidades_routes.py
@@ -1,17 +1,19 @@
 # app/routes/entidades_routes.py
 
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from app.controllers.entidades_controller import EntidadesController
+from ..app import auth
 
 entidades_bp = Blueprint("entidades", __name__)
-
 entidades_controller = EntidadesController()
 
 @entidades_bp.route("/entidades", methods=["GET"])
+@auth.login_required
 def get_entidades():
     return entidades_controller.get_all_entidades()
 
 @entidades_bp.route("/entidades", methods=["POST"])
+@auth.login_required
 def create_entidad():
     data = request.get_json()
     return entidades_controller.create_entidad(data)
